9e91d365b1552d61e4c8a670934f6172353c8caf
Davron01~ <d.mirabdullayev@mail.ru>
2023-05-16 10:41:33 +0500

    start typescript

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import './App.css';
+
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <div className="wrap">
+        <span className="heading">Наша пицерия</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
